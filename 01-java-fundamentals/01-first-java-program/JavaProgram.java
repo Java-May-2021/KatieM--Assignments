52768f25c6565a6eca27ea6c424fadcc47dc5cb9
Katarzyna Misielak <misielakkatarzyna@yahoo.com>
2021-05-11 18:08:13 -0500

    first commit

---
+++ 01-java-fundamentals/01-first-java-program/JavaProgram.java
@@ -0,0 +1,7 @@
+public class JavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My Name is Katie");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Rzeszow, Poland");
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean legC = new Pythagorean();
+        System.out.println(legC.calculateHypotenuse(4,5));
+    }
+}
